62b1851068b0eb596c22036d014deaa8ec679a01
Kanishak <kanukmr0@gmail.com>
2021-10-04 19:57:20 +0530

    changed

--- components/camera/index.js
+++ components/camera/index.js
@@ -11,7 +11,7 @@ const videoConstraints = {
   };
   
   export default function WebcamCapture () {
-    const [images , setImages] = useState([])
+    const [images , setImages] = useState([{}])
     const webcamRef = React.useRef(null);
 
     const capture = React.useCallback(
@@ -22,7 +22,7 @@ const videoConstraints = {
                 'eng',
                 { logger: m => console.log(m) }
               ).then(({ data: { text } }) => {
-                setImages([...images,text])
+                setImages([...images,{img_src : imageSrc , text}])
                 console.log(text);
               })
         },
@@ -32,18 +32,17 @@ const videoConstraints = {
         
     return (
       <>
-
         <Webcam
           audio={false}
           height={720}
           ref={webcamRef}
-          screenshotFormat="image/jpeg"
+          screenshotFormat="image/png"
           width={1280}
           videoConstraints={videoConstraints}
         />
-        <button onClick={capture}>Capture photo</button>
-        { images.map((img) => {
-          return <p key={img}> 1 . {img}</p>  
+        <button type="button" onClick={capture}>Capture photo</button>
+        { images.map(({img_src ,text}) => {
+          return <><img key={img_src} src={img_src} /> <p>{text}</p> </>
       })
     }
       </>
